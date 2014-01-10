6fe6003a2a730adfee816c1bfb66d2bcf53ee5cf
Nicola Montecchio <nicola.montecchio@gmail.com>
2014-01-10 11:54:51 -0500

    added examples for matrices

--- a.cpp
+++ a.cpp
@@ -1,15 +1,24 @@
-extern "C" 
+extern "C"
 {
-	float sum_float_numpy_array(float* a, int n);
+  float sum_float_numpy_array(float* a, int n);
+  float biggest_abs_eigenvalue(float* A, int n); // biggest eigenvalue of square (n x n) matrix A
 }
 
 
 #include <Eigen/Core>
+#include <Eigen/Eigenvalues>
 using namespace Eigen;
 
 
 float sum_float_numpy_array(float* a, int n)
 {
-	Map<VectorXf> bubi(a, n);
-	return bubi.sum();
+  Map<VectorXf> bubi(a, n);
+  return bubi.sum();
+}
+
+
+float biggest_abs_eigenvalue(float*  A, int n)
+{
+  Map<MatrixXf> m(A, n, n);
+  return m.eigenvalues().cwiseAbs().array().maxCoeff();
 }
--- aaa.py
+++ aaa.py
@@ -7,10 +7,22 @@ if __name__ == '__main__':
     print 'testing'
     a = np.array(np.random.random(45), dtype=np.float32)
     lib = C.CDLL('libeigentestpy.dylib')
-    f_load_index = lib.sum_float_numpy_array
-    f_load_index.argtypes = [np.ctypeslib.ndpointer(dtype = np.float32), C.c_int]
-    f_load_index.restype = C.c_float
+    f_eigen_sum = lib.sum_float_numpy_array
+    f_eigen_sum.argtypes = [np.ctypeslib.ndpointer(dtype = np.float32), C.c_int]
+    f_eigen_sum.restype = C.c_float
     print 'python says sum is:', a.sum()
-    print 'eigen instead says:', f_load_index(a, len(a))
+    print 'eigen instead says:', f_eigen_sum(a, len(a))
 
+    print
 
+    a = np.array([[1,5,23,21],
+                  [3,-2,11,4],
+                  [4,21,-44,-23],
+                  [-12,11,10,4]], dtype=np.float32)
+    ata = np.dot(a.T, a)
+    print ata
+    print np.max(np.abs(np.linalg.eig(ata)[0]))
+    f_eigen_maxabseig = lib.biggest_abs_eigenvalue
+    f_eigen_maxabseig.argtypes = [np.ctypeslib.ndpointer(dtype = np.float32), C.c_int]
+    f_eigen_maxabseig.restype = C.c_float
+    print f_eigen_maxabseig(ata, ata.shape[0])
